33bc0001021e2fd0f653dbb9bbb57c50e9584772
Kader Bouyakoub <bkader@mail.com>
2018-01-14 14:48:39 +0100

    Remving __() function and using lang() instead.

--- src/skeleton/views/admin/layouts/default.php
+++ src/skeleton/views/admin/layouts/default.php
@@ -11,7 +11,7 @@
 
 		<div id="navbar" class="navbar-collapse collapse">
 			<ul class="nav navbar-nav navbar-right">
-				<li><?php echo anchor('', __('view_site')) ?></li>
+				<li><?php echo anchor('', lang('view_site')) ?></li>
 				<li class="user-menu dropdown">
 					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><?php echo $c_user->first_name; ?><?php echo user_avatar(24, $c_user->id, 'class="img-circle"'); ?></a>
 					<ul class="dropdown-menu">
@@ -30,17 +30,20 @@
 	<div class="row">
 		<div class="col-xs-12 col-sm-3 col-md-2 sidebar">
 			<ul class="nav nav-sidebar">
-				<li<?php echo (get_the_module() == null) ? ' class="active"' : '' ?>><?php echo admin_anchor('', __('dashboard')) ?></li>
-				<li<?php echo is_module('users') ? ' class="active"' : '' ?>><?php echo admin_anchor('users', __('users')) ?></li>
-				<li<?php echo is_module('themes') ? ' class="active"' : '' ?>><?php echo admin_anchor('themes', __('themes')) ?></li>
-				<li<?php echo is_module('menus') ? ' class="active"' : '' ?>><?php echo admin_anchor('menus', __('menus')) ?></li>
-				<li<?php echo is_module('plugins') ? ' class="active"' : '' ?>><?php echo admin_anchor('plugins', __('plugins')) ?></li>
-				<li<?php echo is_module('settings') ? ' class="active"' : '' ?>><?php echo admin_anchor('settings', __('settings')) ?></li>
+				<li<?php echo (get_the_module() == null) ? ' class="active"' : '' ?>><?php echo admin_anchor('', lang('dashboard')) ?></li>
+				<li<?php echo is_module('users') ? ' class="active"' : '' ?>><?php echo admin_anchor('users', lang('users')) ?></li>
+				<li<?php echo is_module('themes') ? ' class="active"' : '' ?>><?php echo admin_anchor('themes', lang('themes')) ?></li>
+				<li<?php echo is_module('menus') ? ' class="active"' : '' ?>><?php echo admin_anchor('menus', lang('menus')) ?></li>
+				<li<?php echo is_module('plugins') ? ' class="active"' : '' ?>><?php echo admin_anchor('plugins', lang('plugins')) ?></li>
+				<li<?php echo is_module('settings') ? ' class="active"' : '' ?>><?php echo admin_anchor('settings', lang('settings')) ?></li>
 			</ul>
 		</div>
 		<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
 			<?php the_content(); ?>
-			<div class="footer"><?php echo anchor('', get_option('site_name')) ?>. &copy; Copyright <?php echo date('Y') ?>. RT: <strong>{elapsed_time}</strong>. TT: <strong>{theme_time}</strong>.</div>
+			<div class="footer">
+				<hr>
+				<p class="text-center"><?php echo anchor('', get_option('site_name')) ?>. &copy; Copyright <?php echo date('Y') ?>. RT: <strong>{elapsed_time}</strong>. TT: <strong>{theme_time}</strong>.<br ><?php _e('created_by'); ?> <a href="https://github.com/bkader" target="_blank">Kader Bouyakoub</a></p>
+			</div>
 		</div>
 	</div><!--/.row-->
 </div><!--/.container-fluid-->
