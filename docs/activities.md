355b4cf6cee80e87091ee943cdfdc223bcfa9c15
Kader Bouyakoub <bkader@mail.com>
2018-01-16 13:05:57 +0100

    Updating activities library and adding activities documentation.

--- docs/activities.md
+++ docs/activities.md
@@ -1,15 +1,207 @@
-# Bkader Activities Driver (Bkader_activities)
+# Activities
 
-You can find this file inside your __application/libraries/Bkader__ folder.
+* [Definition](#markdown-header-what-are-metadata)  
+* [Creating Activities](#markdown-header-creating-activities)
+* [Updating Activities](#markdown-header-updating-activities)
+* [Deleting Activities](#markdown-header-deleting-activities)
+* [Retrieving Activities](#markdown-header-retrieving-activities)
 
-## Library Methods.
+### Definition:
+Activities are a way to track users actions and reactions on the website. Think of them as history log where is stored who did what, when and where.  
 
-This class has only few methods.
+Take a look at activities table:  
 
-### initiliaze()
+![Activities Table](table_activities.png)  
 
-This method is called on the parent's __constructor__. It was added only to make sure the class is initialized and off course to make its function helper available on the whole application.
+All you really need to provide as information are **user_id** and the **activity** string.  
 
-### log_activiti($user_id, $activity)
+### Creating Activities:
+To log a new activity, you have two options (methods):  
+```php
+$this->app->activities->create(array()); // MUST be an array.
+$this->app->activities->log_activity($user_id, $activity);
+// Or you can user the helper:
+log_activity($user_id, $activity);
+```
+You can find some examples if you take a look at the provided **users** module (**Auth** library) where users interactions with the module are logged. Here is an example of activity log at user's login:  
+```php
+// After a successful login:
+log_activity($user_id, 'logged in'); // That's all.
+```
+Use your imagination and make acitivity history suit your needs. Let's assume that I have created a **CMS** using this CodeIgniter skeleton. What I can do it to record, for insteance, who published post **#111*** and then who edited it. Let's assume again that I have a module called **blog**, the controller to publish posts is named **posts** and the method is **create**. Here is what I can do:
+```php
+// After the user #73 created the post #111, I use their IDs:
+log_activity(73, 111);
+// That's all. Everything else is automatically added.
+```
+In my activities table, I will see the following:
 
-Like its name says, it simply records a new user activity. It will automatically hold the module's name if any, the controller's name and the method's name for you. It also holds the ip address of the user.
+| user_id | module | controller | method | activity |
+| -- | -- | -- | -- | -- |
+| 73 | blog | posts | create | 111 |  
+
+This can be translated as _the user #73 published a new post #111_.  
+
+Now when another user (i.e **#23**) edits the post (the method is **edit**), at a successful updated, I can log the activity like so:
+```php
+log_activity(23, 111); // That's all.
+```
+So in my activities table I would have:  
+
+| user_id | module | controller | method | activity |
+| -- | -- | -- | -- | -- |
+| 23 | blog | posts | edit| 111 |  
+
+This can be translated later as : _the user #23 edited the post #111_.
+
+### Updating Activites:
+In fact, because they are automatic, it is better not to update them at all. This way you garanty you don't mess up with what was previously logged.  
+But there are some cases where you want to update them. For example: lets assume you changed the controller's name **posts** of the module **blog** to something else, let's say **blog**. Now you really want to update all of this module's activities **controller** column to use blog instead of posts. This why we have added **update** and **update_by** method (this time with no helpers).
+```php
+// To update a single activity knowing its ID.
+$this->app->activities->update($id, array $data);
+// Or you can use the helper:
+update_activity($id, array $data);
+
+// To update a single or multiple ones by arbitrary WHERE clause:
+$this->app->activities->update_by(array $where, array $data);
+// Or you can use the helper:
+update_activity_by(array $where, array $data);
+// Or its alias:
+update_activities(array $where, array $data);
+
+// To update all activities, user $data instead of $where:
+$this->app->activities->update_by(array $data);
+update_activity_by(array $data);
+update_activities(array $data);
+```
+Let's stick to the example given above and say I want to change the blog controller from **posts** to **blog** for all activities, I can do this:
+```php
+$this->app->activities->update_by(
+	array(	// This is the WHERE clause.
+		'module'     => 'blog',
+		'controller' => 'posts',
+	),
+	array('controller' => 'blog'),
+);
+// Or the helper
+update_activity_by(
+	array(	// This is the WHERE clause.
+		'module'     => 'blog',
+		'controller' => 'posts',
+	),
+	array('controller' => 'blog'),
+);
+// Or its alias:
+update_activities(
+	array(	// This is the WHERE clause.
+		'module'     => 'blog',
+		'controller' => 'posts',
+	),
+	array('controller' => 'blog'),
+);
+```
+
+### Deleting Activities:
+There are two methods and their helpers to delete activities. Here some example to demonstrate them.  
+
+In order to delete a single activity assuming that you know its **ID**:
+```php
+$this->app->activities->delete($id);
+// Or using the helper:
+delete_activity($id);
+```
+The method below deletes a single activity by arbitrary _WHERE_ clause. But it is also used to delete multiple activities as well and if used without arguments, it will delete **ALL** activities.
+```php
+$this->app->activities->delete_by($field, $match);
+// Or its helper and alias:
+delete_activity_by($field, $match);
+delete_activities($field, $match);
+```
+Below are some examples to explain better:
+```php
+// Example #1: Delete all activities of user #23
+elete_activities('user_id', 23);
+
+// Example #2: Delete activites of user #23 on module "blog".
+delete_activities(array(
+	'user_id' => 23,
+	'module'  => 'blog',
+));
+
+// Example #3: 
+// Delete user #23 activites on modules "blog" and"messages":
+delete_activities(array(
+	'user_id' => 23,
+	'module'  => ['blog', 'messages']
+));
+
+// Example #4: Delete activities older than 6 months:
+delete_activities('created_at <', MONTH_IN_SECONDS * 6);
+```
+**Note**: Because "_delete_activities_" and "_delete_activity_by_" do the same thing, I only used the first one in the examples above.
+
+### Retrieving Activities:
+Here is how you can retrieve activities:
+```php
+// If you know the activity's ID:
+$this->app->activities->get($id);
+// Or the helper:
+get_activity($id);
+
+// To retrieve single activity by arbitrary WHERE clause.
+$this->app->activities->get_by($field, $match);
+get_activity($field, $match);
+
+// To retrieve multiple activities by arbitrary WHERE clause.
+$this->app->activities->get_many($field, $match);
+// Of the helper:
+get_activities($field, $match);
+```
+Sticking to the blog example and for a better explanation, here are few examples:
+```php
+// Example #1:
+// Retrieve the activity where user#73 created the post#111
+$activty = $this->app->activities->get_by(array(
+	'user_id'    => 73,
+	'module'     => 'blog',
+	'controller' => 'posts',
+	'method'     => 'create',
+	'activity'   => 111,
+));
+// To access the object:
+echo $activity->ip_address;
+echo date('Y-m-d H:i:s, $activity->created_at);
+
+// Example #2: retrieve all posts publishing activities.
+$activities = get_activities(array(
+	'module'     => 'blog',
+	'controller' => 'posts',
+	'method'     => 'create',
+));
+
+// Example #3: get a list of all post#111 updated.
+$updates = get_activities(array(
+	'module'     => 'blog',
+	'controller' => 'posts',
+	'method'     => 'edit',
+	'activity'   => 111,
+));
+```
+In the example **#4**, we will have an array of activities objects that you can use later to retrieve all users who made updates on the selected post (**#111***). Here is how I personally proceed after:
+```php
+// We have collected all activities, now we get users.
+$users_ids = array();
+
+// Now we fill our $uses_ids array.
+foreach ($activities as $activity)
+{
+	$users_ids[] = $activity->user_id;
+}
+
+// Now that we have users IDs array, let's get users.
+$users = $this->app->users->get_many('id', $users_ids);
+// Or the helper:
+get_users('id', $users_ids);
+```
+As you can see, you are not as limited as you think you are. Explore the library for more details.
--- src/skeleton/libraries/Bkader/drivers/Bkader_activities.php
+++ src/skeleton/libraries/Bkader/drivers/Bkader_activities.php
@@ -60,32 +60,241 @@ class Bkader_activities extends CI_Driver
 	 */
 	public function initialize()
 	{
-		// Make sure to load activities model.
-		$this->ci->load->model('bkader_activities_m');
-
 		log_message('info', 'Bkader_activities Class Initialized');
 	}
 
-	// --------------------------------------------------------------------
+	// ------------------------------------------------------------------------
+	// CRUD Interface.
+	// ------------------------------------------------------------------------
 
 	/**
-	 * Magic __call method to use activities model methods too.
+	 * Create a new activity log.
 	 * @access 	public
-	 * @param 	string 	$method 	the method's name.
-	 * @param 	array 	$params 	arguments to pass to method.
-	 * @return 	mixed 	depends on the called method.
+	 * @param 	array 	$data 	Array of data to insert.
+	 * @return 	int 	The new activity ID if created, else FALSE.
 	 */
-	public function __call($method, $params = array())
+	public function create(array $data = array())
 	{
-		if (method_exists($this->ci->bkader_activities_m, $method))
+		// Without $data, nothing to do.
+		if (empty($data))
 		{
-			return call_user_func_array(
-				array($this->ci->bkader_activities_m, $method),
-				$params
-			);
+			return FALSE;
 		}
 
-		throw new BadMethodCallException("No such method ".get_called_class()."::{$method}().");
+		// Multiple activities?
+		if (isset($data[0]) && is_array($data[0]))
+		{
+			$ids = array();
+			foreach ($data as $_data)
+			{
+				$ids[] = $this->create($_data);
+			}
+
+			return $ids;
+		}
+
+		// Let's complete some data.
+		(isset($data['module']))     OR $data['module']     = $this->ci->router->fetch_module();
+		(isset($data['controller'])) OR $data['controller'] = $this->ci->router->fetch_class();
+		(isset($data['method']))     OR $data['method']     = $this->ci->router->fetch_method();
+		(isset($data['created_at'])) OR $data['created_at'] = time();
+		(isset($data['ip_address'])) OR $data['ip_address'] = $this->ci->input->ip_address();
+
+		// Proceed to creation and return the ID.
+		$this->ci->db->insert('activities', $data);
+		return $this->ci->db->insert_id();
+	}
+
+	// ------------------------------------------------------------------------
+
+	/**
+	 * Retrieve a single activity by its ID.
+	 * @access 	public
+	 * @param 	int 	$id 	The activity's ID.
+	 * @return 	object if found, else NULL.
+	 */
+	public function get($id)
+	{
+		return $this->get_by('id', $id);
+	}
+
+	// ------------------------------------------------------------------------
+
+	/**
+	 * Retrieve a single activity by arbitrary WHERE clause.
+	 * @access 	public
+	 * @param 	mixed 	$field 	Column name or associative array.
+	 * @param 	mixed 	$match 	Comparison value, array or NULL.
+	 * @return 	object if found, else NULL.
+	 */
+	public function get_by($field, $match = NULL)
+	{
+		// The WHERE clause depends on $field and $match.
+		(is_array($field)) OR $field = array($field => $match);
+
+		foreach ($field as $key => $val)
+		{
+			if (is_array($field))
+			{
+				$this->ci->db->where_in($key, $val);
+			}
+			else
+			{
+				$this->ci->db->where($key, $val);
+			}
+		}
+
+		// Order activities and retrieve the last one.
+		return $this->ci->db
+			->order_by('id', 'DESC')
+			->limit(1)
+			->get('activities')->row();
+	}
+
+	// ------------------------------------------------------------------------
+
+	/**
+	 * Retrieve multiple activities by arbitrary WHERE clause.
+	 * @access 	public
+	 * @param 	mixed 	$field 	Column name or associative array.
+	 * @param 	mixed 	$match 	Comparison value, array or NULL.
+	 * @return 	array of objects if found, else NULL.
+	 */
+	public function get_many($field = NULL, $match = NULL)
+	{
+		if ( ! empty($field))
+		{
+			// Turn things into an array.
+			(is_array($field)) OR $field = array($field => $match);
+
+			foreach ($field as $key => $val)
+			{
+				if (is_array($val))
+				{
+					$this->ci->db->where_in($key, $val);
+				}
+				else
+				{
+					$this->ci->db->where($key, $val);
+				}
+			}
+		}
+
+		return $this->ci->db
+			->order_by('id', 'DESC')
+			->get('activities')
+			->result();
+	}
+
+	// ------------------------------------------------------------------------
+
+	/**
+	 * Update a single entity by it's ID.
+	 * @access 	public
+	 * @param 	int 	$id 	The activity's ID.
+	 * @param 	array 	$data 	Array of data to update.
+	 * @return 	boolean
+	 */
+	public function update($id, array $data = array())
+	{
+		return $this->update_by(array('id', $id), $data);
+	}
+
+	// ------------------------------------------------------------------------
+
+	/**
+	 * Update a single or multiple activities by arbitrary WHERE clause.
+	 * @access 	public
+	 * @return 	boolean
+	 */
+	public function update_by()
+	{
+		// Collect arguments first and make sure there are some.
+		$args = func_get_args();
+		if (empt($args))
+		{
+			return FALSE;
+		}
+
+		// Data to set is always the last argument.
+		$data = array_pop($args);
+		if ( ! is_array($data) OR empt($data))
+		{
+			return FALSE;
+		}
+
+		// Start updating/
+		$this->ci->db->update($data);
+
+		// If there are arguments left, use the as WHERE clause.
+		if ( ! empty($args))
+		{
+			// Get rid of nasty deep array.
+			(is_array($args[0])) && $args = $args[0];
+
+			foreach ($args as $key => $val)
+			{
+				if (is_array($val))
+				{
+					$this->ci->db->where_in($key, $val);
+				}
+				else
+				{
+					$this->ci->db->where($key, $val);
+				}
+			}
+		}
+
+		// Proceed to update.
+		$this->ci->db->update('activities');
+		return ($this->ci->db->affected_rows() > 0);
+	}
+
+	// ------------------------------------------------------------------------
+
+	/**
+	 * Delete a single activity by its ID.
+	 * @access 	public
+	 * @param 	int 	$id 	The activity's ID.
+	 * @return 	boolean
+	 */
+	public function delete($id)
+	{
+		return $this->delete_by('id', $id);
+	}
+
+	// ------------------------------------------------------------------------
+
+	/**
+	 * Delete multiple activities by arbitrary WHERE clause.
+	 * @access 	public
+	 * @param 	mixed 	$field 	Column name or associative array.
+	 * @param 	mixed 	$match 	Comparison value, array or NULL.
+	 * @return 	boolean
+	 */
+	public function delete_by($field = null, $match = null)
+	{
+		if ( ! empty($field))
+		{
+			// Turn things into an array.
+			(is_array($field)) OR $field = array($field => $match);
+
+			foreach ($field as $key => $val)
+			{
+				if (is_array($val))
+				{
+					$this->ci->db->where_in($key, $val);
+				}
+				else
+				{
+					$this->ci->db->where($key, $val);
+				}
+			}
+		}
+
+		// Proceed to deletion.
+		$this->ci->db->delete('activities');
+		return ($this->ci->db->affected_rows() > 0);
 	}
 
 	// --------------------------------------------------------------------
@@ -100,18 +309,15 @@ class Bkader_activities extends CI_Driver
 	 */
 	public function log_activity($user_id, $activity)
 	{
+		// Both user's ID and activity are required.
 		if (empty($user_id) OR empty($activity))
 		{
-			return false;
+			return FALSE;
 		}
 
-		return $this->ci->bkader_activities_m->insert(array(
-			'user_id'    => $user_id,
-			'module'     => $this->ci->router->fetch_module(),
-			'controller' => $this->ci->router->fetch_class(),
-			'method'     => $this->ci->router->fetch_method(),
-			'activity'   => strval($activity),
-			'ip_address' => $this->ci->input->ip_address(),
+		return $this->create(array(
+			'user_id'  => $user_id,
+			'activity' => $activity,
 		));
 	}
 
@@ -130,5 +336,148 @@ if ( ! function_exists('log_activity'))
 	function log_activity($user_id, $activity)
 	{
 		return get_instance()->app->activities->log_activity($user_id, $activity);
+	}
+}
+
+// ------------------------------------------------------------------------
+
+if ( ! function_exists('get_activity'))
+{
+	/**
+	 * Retrieve a single activity by its ID or arbitrary WHERE clause.
+	 * @access 	public
+	 * @param 	mixed 	$field 	ID, column name or associative array.
+	 * @param 	mixed 	$match 	Comparison value, array or NULL.
+	 * @return 	object if found, else NULL.
+	 */
+	function get_activity($field, $match = null)
+	{
+		// In case of using the ID.
+		if (is_numeric($field))
+		{
+			return get_instance()->app->activities->get($field);
+		}
+
+		return get_instance()->app->activities->get_by($field, $match);
+	}
+}
+
+// ------------------------------------------------------------------------
+
+if ( ! function_exists('get_activities'))
+{
+	/**
+	 * Retrieve multiple activities by arbitrary WHERE clause or 
+	 * retrieve all activities if no arguments passed.
+	 * @access 	public
+	 * @param 	mixed 	$field 	Column name or associative array.
+	 * @param 	mixed 	$match 	Comparison value, array or NULL.
+	 * @return 	array of objects if found, else NULL.
+	 */
+	function get_activities($field = null, $match = null)
+	{
+		return get_instance()->app->activities->get_many($field, $match);
+	}
+}
+
+// ------------------------------------------------------------------------
+
+if ( ! function_exists('update_activity'))
+{
+	/**
+	 * Update a single activity by its ID.
+	 * @param 	int 	$id 	The activity's ID.
+	 * @param 	array 	$data 	Array of data to update.
+	 * @return 	boolean
+	 */
+	function update_activity($id, array $data = array())
+	{
+		return get_instance()->app->activities->update($id, $data);
+	}
+}
+
+// ------------------------------------------------------------------------
+
+if ( ! function_exists('update_activity_by'))
+{
+	/**
+	 * Update a single activity, multiple activities by arbitrary WHERE 
+	 * clause, or even update all activities.
+	 * @return 	boolean
+	 */
+	function update_activity_by()
+	{
+		return call_user_func_array(
+			array(get_instance()->app->activities, 'update_by'),
+			func_get_args()
+		);
+	}
+}
+
+// ------------------------------------------------------------------------
+
+if ( ! function_exists('update_activities'))
+{
+	/**
+	 * Update a single activity, multiple activities by arbitrary WHERE 
+	 * clause, or even update all activities.
+	 * @return 	boolean
+	 */
+	function update_activities()
+	{
+		return call_user_func_array(
+			array(get_instance()->app->activities, 'update_by'),
+			func_get_args()
+		);
+	}
+}
+
+// ------------------------------------------------------------------------
+
+if ( ! function_exists('delete_activity'))
+{
+	/**
+	 * Delete a single activity by its ID.
+	 * @access 	public
+	 * @param 	int 	$id 	The activity's ID.
+	 * @return 	boolean
+	 */
+	function delete_activity($id)
+	{
+		return get_instance()->app->activities->delete($id);
+	}
+}
+
+// ------------------------------------------------------------------------
+
+if ( ! function_exists('delete_activity_by'))
+{
+	/**
+	 * Delete a single activity, multiple activities by arbitrary WHERE
+	 * clause or even delete all activities.
+	 * @param 	mixed 	$field
+	 * @param 	mixed 	$match
+	 * @return 	boolean
+	 */
+	function delete_activity_by($field = null, $match = null)
+	{
+		return get_instance()->app->activities->delete_by($field, $match);
+	}
+}
+
+// ------------------------------------------------------------------------
+
+if ( ! function_exists('delete_activities'))
+{
+	/**
+	 * Delete a single activity, multiple activities by arbitrary WHERE
+	 * clause or even delete all activities.
+	 * @param 	mixed 	$field
+	 * @param 	mixed 	$match
+	 * @return 	boolean
+	 */
+	function delete_activities($field = null, $match = null)
+	{
+		return get_instance()->app->activities->delete_by($field, $match);
 	}
 }
