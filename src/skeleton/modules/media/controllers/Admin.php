2c925240ec7daa341322eee6741890451ffe1ed2
Kader Bouyakoub <bkader@mail.com>
2018-02-01 07:39:54 +0100

    Formatted and commented code.

--- src/skeleton/modules/media/controllers/Admin.php
+++ src/skeleton/modules/media/controllers/Admin.php
@@ -75,12 +75,15 @@ class Admin extends Admin_Controller
 	 */
 	public function index()
 	{
-		// Add dropzone js.
-		$this->load_jquery_ui();
+		// Make sure to load Dropzone CSS and JS files.
 		$this->theme
 			->add('css', get_common_url('css/dropzone'), 'dropzone')
 			->add('js', get_common_url('js/dropzone'), 'dropzone');
 
+		/**
+		 * Here we are preparing the upload URL and the
+		 * inline dropzone handler script.
+		 */
 		$upload_url = admin_url('media/upload');
 		$dropzone =<<<EOT
 \n\t<script>
@@ -97,18 +100,27 @@ class Admin extends Admin_Controller
 	});
 	</script>
 EOT;
-
-
 		$this->theme->add_inline('js', $dropzone);
 
+		// Prepare form validation.
 		$this->prep_form();
+
+		// Load all media from database.
 		$data['media'] = $this->kbcore->media->get_all();
 
+		// Set page title and load view.
 		$this->theme
 			->set_title(lang('media_library'))
 			->render($data);
 	}
 
+	// ------------------------------------------------------------------------
+
+	/**
+	 * Media upload handler method.
+	 * @access 	public
+	 * @return 	void
+	 */
 	public function upload()
 	{
 		// Make sure to create the upload folder if not found.
@@ -117,25 +129,32 @@ EOT;
 			mkdir(FCPATH.'content/uploads/'.date('Y/m/'), 0777);
 		}
 
+		// We prepare upload library configuration.
 		$config['upload_path']      = './content/uploads/'.date('Y/m/');
 		$config['allowed_types']    = 'gif|jpg|png';
-		// $config['max_size']         = '100';
-		// $config['max_width']        = '1024';
-		// $config['max_height']       = '768';
 		$config['file_ext_tolower'] = true;
 		$config['encrypt_name']     = true;
 		$config['remove_spaces']    = true;
 
+		// Load upload library.
 		$this->load->library('upload', $config);
 
-		if ( ! $this->upload->do_upload('file')){
+		// An error occured? Return it to browser.
+		if ( ! $this->upload->do_upload('file'))
+		{
 			echo $this->upload->display_errors();
 			die();
 		}
+		// File uploaded? Proceed.
 		else
 		{
+			// Collect data returned by upload library.
 			$data = $this->upload->data();
 
+			/**
+			 * Here we are preparing the data of the new attachment
+			 * details. Yes, media are also stored in database.
+			 */
 			$media = array(
 				'username' => base_url('content/uploads/'.date('Y/m/').$data['file_name']),
 				'name' => $data['raw_name'],
@@ -150,19 +169,30 @@ EOT;
 					'file_mime' => $data['file_type'],
 				),
 			);
+
+			// Proceed to creating media object.
 			$this->kbcore->media->create($media);
 
-			// Proceed to crop and resizes.
-			// Theme sizes.
+			/**
+			 * We retrieve all current theme's images sizes from
+			 * database. If there are any, we creation thumbnails.
+			 */
 			$sizes = get_option('theme_images_'.get_option('theme'));
 			if ( ! empty($sizes))
 			{
+				// Load Image_lib library.
 				$this->load->library('image_lib');
 
+				// Loop through all sizes and prepare for creation.
 				foreach ($sizes as $name => $details)
 				{
+					/**
+					 * Because we are in a foreach loop, we make sure to
+					 * clear the library to avoid issues.
+					 */
 					$this->image_lib->clear();
 
+					// Prepare image library configuration.
 					$config['upload_path']    = './content/uploads/'.date('Y/m/');
 					$config['image_library']  = 'gd2';
 					$config['source_image']   = $data['full_path'];
@@ -172,19 +202,15 @@ EOT;
 					$config['create_thumb']   = true;
 					$config['thumb_marker']   = '-'.$details['width'].'x'.$details['height'];
 
+					// Initialize library and resize the image.
 					$this->image_lib->initialize($config);
 					$this->image_lib->resize();
 				}
 			}
 
-			echo "success";
+			// Simple message that's is return to use.
+			echo lang('media_upload');
+			die();
 		}
-		// if ( ! empty($_FILES))
-		// {
-		// 	$tempFile = $_FILES['file']['tmp_name'];
-		// 	$targetPath = FCPATH.'content/uploads/';
-		// 	$targetFile =  $targetPath. $_FILES['file']['name'];  //5
-		// 	move_uploaded_file($tempFile,$targetFile); //6
-		// }
 	}
 }
