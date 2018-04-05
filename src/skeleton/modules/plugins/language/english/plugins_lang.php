6c9c3040d3b522867d48fae4ebde00869c84f477
Kader Bouyakoub <bkader@mail.com>
2018-04-05 19:08:18 +0100

    Renamed file.

--- src/skeleton/modules/plugins/language/english/plugins_lang.php
+++ src/skeleton/modules/plugins/language/english/plugins_lang.php
@@ -69,6 +69,7 @@ $lang['plugins_deactivate_error']   = 'Unable to deactivate plugin.';
 
 $lang['plugins_delete_success'] = 'Plugin successfully deleted.';
 $lang['plugins_delete_error']   = 'Unable to delete plugin.';
+$lang['plugins_delete_confirm']   = 'Are you sure you want to delete %s at its data?';
 
 $lang['plugin_settings_success'] = 'Plugin settings successfully updated.';
 $lang['plugin_settings_error']   = 'Unable to update plugin settings.';
