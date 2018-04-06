4a63ee0b89766602d73dbe595ec1206b9f812720
Kader Bouyakoub <bkader@mail.com>
2018-04-06 14:34:53 +0100

    Those lines were there for testing purpose, we deleted them.

--- public/content/themes/default/templates/welcome/index.php
+++ public/content/themes/default/templates/welcome/index.php
@@ -10,12 +10,6 @@
 <p>The corresponding controller for this page is found at:</p>
 <code>application/controllers/Welcome.php</code>
 
-<div class="row">
-	<div class="col-md-3">
-		<?php echo img(get_upload_url('85818.jpg'), 'data-action="zoom" class="img-responsive"') ?>
-	</div>
-</div>
-
 <p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
 
 <p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
