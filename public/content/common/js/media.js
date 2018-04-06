3fdc4794abade740fad3a46bd7d8ba5d78b0b67f
Kader Bouyakoub <bkader@mail.com>
2018-04-06 03:22:10 +0100

    Media JS file was cleaned and made better.

--- public/content/common/js/media.js
+++ public/content/common/js/media.js
@@ -1,10 +1,89 @@
-(function ($, bootbox, axios, undefined) {
+/*!
+ * Skeleton Media - v1.3.0 (https://github.com/bkader/skeleton)
+ * Copyright 2018 Kader Bouyakoub (https://github.com/bkader)
+ * Licensed under MIT (https://github.com/bkader/skeleton/blob/develop/LICENSE.md)
+ */
+(function ($, undefined) {
 
-    /* Everything is done when DOM is fully loaded. */
-    $(document).ready(function () {
+    jQuery.noConflict();
+    "use strict";
 
-        var droparea = $('[data-dropzone]');
-        var upload_url = droparea.attr('data-upload-url');
+    // Prepare dropzone and upload URL for later use.
+    var droparea = jQuery('[data-dropzone]'),
+        upload_url = droparea.attr('data-upload-url'),
+        media_url = window.location.origin + window.location.pathname;
+
+    // ========================================================
+    // Media actions: view, close modal, delete and update
+    // ========================================================
+    
+    // Media view button.
+    jQuery(document).on("click", ".media-view", function (e) {
+        e.preventDefault();
+        var that = jQuery(this),
+            href = that.attr("data-href");
+        jQuery("#wrapper").load(href + " #wrapper > *", function() {
+            window.history.pushState({href: href}, '', href);
+            jQuery("#media-modal").modal("show");
+        });
+    });
+
+    // Close media modal.
+    jQuery(document).on("click", ".media-close", function (e) {
+        e.preventDefault();
+        jQuery("#media-modal").modal("hide").on("hidden.bs.modal", function (f) {
+            window.history.pushState({href: media_url}, '', media_url);
+        });
+    });
+
+    // Delete media.
+    jQuery(document).on("click", ".media-delete", function (e) {
+        e.preventDefault();
+        var that = jQuery(this),
+            message = that.attr("data-alert") || "Are you sure?",
+            href = that.attr("data-href");
+
+        if (confirm(message)) {
+            jQuery.ajax({
+                method: "DELETE",
+                type: "DELETE", // Fallback
+                url: href,
+                cache: false,
+                async: true,
+                success: function (data, textStatus, xhr) {
+                    jQuery("#media-modal").modal("hide");
+                    toastr.success(data);
+                    jQuery("#wrapper").load(window.location.href + " #wrapper > *");
+                    window.history.pushState({href: media_url}, '', media_url);
+                },
+                error: function (xhr, textStatus, errorThrown) {
+                    toastr.error(textStatus);
+                }
+            });
+        }
+    });
+
+    // Media update form.
+    jQuery(document).on("submit", "form[data-update]", function (e) {
+        e.preventDefault();
+        var that = jQuery(this),
+            href = that.attr("action");
+
+        var data = {
+            name: jQuery.trim(that.find("#name").val()),
+            description: jQuery.trim(that.find("#description").val())
+        };
+        jQuery.post(href, data, function (response, textStatus, jqXHR) {
+            toastr.success(response);
+        }, "json").fail(function(jqXHR, textStatus, errorThrown) {
+            toastr.error(jqXHR.responseJSON);
+        });
+    });
+
+    // ========================================================
+    // When the DOM is ready.
+    // ========================================================
+    jQuery(document).ready(function () {
 
         var drop = new Dropzone('[data-dropzone]', {
             url: upload_url,
@@ -21,163 +100,6 @@
                 toastr.error(error);
             }
         });
-
-        $(document).on('click', '[data-show]', function (e) {
-            e.preventDefault();
-            var that = $(this),
-                id = that.attr('data-mid'),
-                url = that.attr('ajaxify');
-
-            if ( ! id || ! url) {
-                return;
-            }
-
-            var _modal = document.getElementById('tpl-media-show').innerHTML;
-            var media_model = Handlebars.compile(_modal);
-
-            $.ajax({
-                method: "GET",
-                type: "GET",
-                url: url,
-                dataType: "json",
-                success: function (html) {
-                    $(media_model(JSON.parse(html))).modal();
-                }
-            });
-        });
-
-        $(document).on('submit', 'form[data-update]', function (e) {
-            e.preventDefault();
-            var that = $(this),
-                url = that.attr('action');
-
-            var data = {
-                name: that.find('#name').serializeArray(),
-                description: that.find('#description').serializeArray()
-            };
-
-            $.ajax({
-                method: "PUT",
-                type: "PUT",
-                data: data,
-                url: url,
-                success: function (response) {
-                    console.log(response);
-                    toastr.success(response);
-                },
-                error: function (err) {
-                    console.log(err);
-                }
-            });
-            return false;
-        });
-
-        /**
-         * Delete media action.
-         */
-        $(document).on('click', '[data-delete]', function (e) {
-            e.preventDefault();
-            var that = $(this),
-                id = that.attr("data-mid"),
-                url = that.attr("ajaxify"),
-                message = document.getElementById("tpl-delete-alert").innerHTML,
-                target = $("#media-" + id);
-
-            // Make sure the message is set.
-            message = message || "Are you suer?";
-
-            // Ask the user first.
-            bootbox.confirm({
-                size: "small",
-                message: message,
-                buttons: {
-                    confirm: {className: "btn-danger"},
-                    cancel: {className: "pull-left"}
-                },
-                callback: function (response) {
-                    if (response === true) {
-                        $.ajax({
-                            method: "DELETE",
-                            type: "DELETE", // Fallback
-                            url: url,
-                            cache: false,
-                            async: true,
-                            success: function (data, textStatus, xhr) {
-                                droparea.load(window.location.href + " [data-dropzone] .attachments", function () {
-                                    toastr.success(data);
-                                });
-                            },
-                            error: function (xhr, textStatus, errorThrown) {
-                                toastr.error(textStatus);
-                            }
-                        });
-                    }
-                    bootbox.hideAll();
-                }
-            });
-            // bootbox.confirm(message, function (response) {
-            //     if (response === true) {
-            //         $.ajax({
-            //             method: "GET",
-            //             type: "GET", // Fallback
-            //             url: url,
-            //             cache: false,
-            //             async: true,
-            //             success: function (data, textStatus, xhr) {
-            //                 droparea.load(window.location.href + " [data-dropzone] .attachments");
-            //                 toastr.success(data);
-            //             },
-            //             error: function (xhr, textStatus, errorThrown) {
-            //                 toastr.error(textStatus);
-            //             }
-            //         });
-            //     }
-            //     bootbox.hideAll();
-            // });
-            // if (confirm(message)) {
-            //     $.ajax({
-            //         method: "GET",
-            //         type: "GET", // Fallback
-            //         url: url,
-            //         cache: false,
-            //         async: true,
-            //         success: function (data, textStatus, xhr) {
-            //             droparea.load(window.location.href + " [data-dropzone] .attachments");
-            //             toastr.success(data);
-            //         },
-            //         error: function (xhr, textStatus, errorThrown) {
-            //             toastr.error(textStatus);
-            //         }
-            //     });
-            // }
-            return false;
-        });
-        // $('[data-delete]').on('click', function (e) {
-        //     e.preventDefault();
-        //     var that = $(this),
-        //         id = that.attr('data-delete'),
-        //         url = config.ajaxURL + "/media/delete",
-        //         message = document.getElementById("tpl-delete-alert").innerHTML,
-        //         target = that.parent().parent().parent();
-
-        //     message = message || 'Are you sure?';
-
-        //     if (confirm(message)) {
-        //         $.ajax({
-        //             method: "POST",
-        //             type: "POST", /* Fallback */
-        //             url: url,
-        //             data: {action: "delete", id: id},
-        //             success: function (html) {
-        //                 toastr.success(html);
-        //             },
-        //             error: function (html) {
-        //                 toastr.error(html);
-        //             }
-        //         });
-        //     }
-        //     return false;
-        // });
     });
 
-})(jQuery, bootbox, axios);
+})(jQuery);
