101eb2213327e10c1470f82cf6a37f02b2c4856e
Kader Bouyakoub <bkader@mail.com>
2018-05-12 18:39:00 +0100

    Added French and Arabic to settings controller.

---
+++ skeleton/language/arabic/csk_settings_lang.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * CodeIgniter Skeleton
+ *
+ * A ready-to-use CodeIgniter skeleton  with tons of new features
+ * and a whole new concept of hooks (actions and filters) as well
+ * as a ready-to-use and application-free theme and plugins system.
+ *
+ * This content is released under the MIT License (MIT)
+ *
+ * Copyright (c) 2018, Kader Bouyakoub <bkader@mail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @package 	CodeIgniter
+ * @author 		Kader Bouyakoub <bkader@mail.com>
+ * @copyright	Copyright (c) 2018, Kader Bouyakoub <bkader@mail.com>
+ * @license 	http://opensource.org/licenses/MIT	MIT License
+ * @link 		https://goo.gl/wGXHO9
+ * @since 		2.0.0
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Settings controller language file (Arabic).
+ *
+ * @package 	CodeIgniter
+ * @subpackage 	Skeleton
+ * @category 	Language
+ * @author 		Kader Bouyakoub <bkader@mail.com>
+ * @link 		https://goo.gl/wGXHO9
+ * @copyright 	Copyright (c) 2018, Kader Bouyakoub (https://goo.gl/wGXHO9)
+ * @since 		2.0.0
+ * @version 	2.0.0
+ */
+
+$lang['CSK_SETTINGS']          = 'Settings';
+$lang['CSK_SETTINGS_SETTINGS'] = 'Settings';
+
+// ------------------------------------------------------------------------
+// Tabs sections.
+// ------------------------------------------------------------------------
+$lang['CSK_SETTINGS_SYSTEM_INFORMATION'] = 'معلومات النظام';
+$lang['CSK_SETTINGS_PHP_SETTINGS']       = 'إعدادات PHP';
+$lang['CSK_SETTINGS_PHP_INFO']           = 'معلومات PHP';
+
+// ------------------------------------------------------------------------
+// Table headings.
+// ------------------------------------------------------------------------
+$lang['CSK_SETTINGS_SETTING'] = 'الوضع';
+$lang['CSK_SETTINGS_VALUE']   = 'القيمة';
+
+// ------------------------------------------------------------------------
+// System information.
+// ------------------------------------------------------------------------
+$lang['CSK_SETTINGS_PHP_BUILT_ON']     = 'معلومات نظام التشغيل';
+$lang['CSK_SETTINGS_PHP_VERSION']      = 'نسخة PHP';
+$lang['CSK_SETTINGS_DATABASE_TYPE']    = 'نوع قاعدة البيانات';
+$lang['CSK_SETTINGS_DATABASE_VERSION'] = 'نسخة قاعدة البيانات';
+$lang['CSK_SETTINGS_WEB_SERVER']       = 'السيرفر';
+$lang['CSK_SETTINGS_SKELETON_VERSION'] = 'نسخة سكلتون';
+$lang['CSK_SETTINGS_USER_AGENT']       = 'متصفح المستخدم';
+
+// ------------------------------------------------------------------------
+// PHP Settings.
+// ------------------------------------------------------------------------
+$lang['CSK_SETTINGS_SAFE_MODE']          = 'الوضع الآمن Safe Mode';
+$lang['CSK_SETTINGS_DISPLAY_ERRORS']     = 'عرض الأخطاء';
+$lang['CSK_SETTINGS_SHORT_OPEN_TAG']     = 'الوسوم القصيرة المفتوحة Short Open Tags';
+$lang['CSK_SETTINGS_FILE_UPLOADS']       = 'رفع الملفات';
+$lang['CSK_SETTINGS_MAGIC_QUOTES_GPC']   = 'الإقتباسات السحرية Magic Quotes';
+$lang['CSK_SETTINGS_REGISTER_GLOBALS']   = 'التسجيل العام Register Globals';
+$lang['CSK_SETTINGS_OUTPUT_BUFFERING']   = 'تخزين المخرجات Output Buffering';
+$lang['CSK_SETTINGS_OPEN_BASEDIR']       = 'مجلد الدليل الأساسي المفتوح (الموقع) Open basedir';
+$lang['CSK_SETTINGS_SESSION.SAVE_PATH']  = 'مسار حفظ الجلسة Session save path';
+$lang['CSK_SETTINGS_SESSION.AUTO_START'] = 'بداية تلقائية للجلسة Session auto start';
+$lang['CSK_SETTINGS_DISABLE_FUNCTIONS']  = 'الوظائف المعطلة';
+$lang['CSK_SETTINGS_XML']                = 'تفعيل XML';
+$lang['CSK_SETTINGS_ZLIB']               = 'تفعيل Zlib';
+$lang['CSK_SETTINGS_ZIP']                = 'قابلية ضغط الملفات مفعلة';
+$lang['CSK_SETTINGS_MBSTRING']           = 'تمكين الجمل متعددة البايتات Mbstring Enabled';
+$lang['CSK_SETTINGS_ICONV']              = 'Iconv متاح';
+$lang['CSK_SETTINGS_MAX_INPUT_VARS']     = 'متغيرات الإدخال القصوى';
---
+++ skeleton/language/french/csk_settings_lang.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * CodeIgniter Skeleton
+ *
+ * A ready-to-use CodeIgniter skeleton  with tons of new features
+ * and a whole new concept of hooks (actions and filters) as well
+ * as a ready-to-use and application-free theme and plugins system.
+ *
+ * This content is released under the MIT License (MIT)
+ *
+ * Copyright (c) 2018, Kader Bouyakoub <bkader@mail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @package 	CodeIgniter
+ * @author 		Kader Bouyakoub <bkader@mail.com>
+ * @copyright	Copyright (c) 2018, Kader Bouyakoub <bkader@mail.com>
+ * @license 	http://opensource.org/licenses/MIT	MIT License
+ * @link 		https://goo.gl/wGXHO9
+ * @since 		2.0.0
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Settings controller language file (French).
+ *
+ * @package 	CodeIgniter
+ * @subpackage 	Skeleton
+ * @category 	Language
+ * @author 		Kader Bouyakoub <bkader@mail.com>
+ * @link 		https://goo.gl/wGXHO9
+ * @copyright 	Copyright (c) 2018, Kader Bouyakoub (https://goo.gl/wGXHO9)
+ * @since 		2.0.0
+ * @version 	2.0.0
+ */
+
+$lang['CSK_SETTINGS']          = 'Paramètres';
+$lang['CSK_SETTINGS_SETTINGS'] = 'Paramètres';
+
+// ------------------------------------------------------------------------
+// Tabs sections.
+// ------------------------------------------------------------------------
+$lang['CSK_SETTINGS_SYSTEM_INFORMATION'] = 'Informations système';
+$lang['CSK_SETTINGS_PHP_SETTINGS']       = 'Paramètres PHP';
+$lang['CSK_SETTINGS_PHP_INFO']           = 'Informations PHP';
+
+// ------------------------------------------------------------------------
+// Table headings.
+// ------------------------------------------------------------------------
+$lang['CSK_SETTINGS_SETTING'] = 'Paramètre';
+$lang['CSK_SETTINGS_VALUE']   = 'Valeur';
+
+// ------------------------------------------------------------------------
+// System information.
+// ------------------------------------------------------------------------
+$lang['CSK_SETTINGS_PHP_BUILT_ON']     = 'PHP exécuté sur';
+$lang['CSK_SETTINGS_PHP_VERSION']      = 'Version de PHP';
+$lang['CSK_SETTINGS_DATABASE_TYPE']    = 'Type de la base de données';
+$lang['CSK_SETTINGS_DATABASE_VERSION'] = 'Version de la base de données';
+$lang['CSK_SETTINGS_WEB_SERVER']       = 'Serveur web';
+$lang['CSK_SETTINGS_SKELETON_VERSION'] = 'Version Skeleton';
+$lang['CSK_SETTINGS_USER_AGENT']       = 'Navigateur';
+
+// ------------------------------------------------------------------------
+// PHP Settings.
+// ------------------------------------------------------------------------
+$lang['CSK_SETTINGS_SAFE_MODE']          = 'Safe Mode (mode de sécurité PHP)';
+$lang['CSK_SETTINGS_DISPLAY_ERRORS']     = 'Display Errors (afficher les erreurs)';
+$lang['CSK_SETTINGS_SHORT_OPEN_TAG']     = 'Short open tags (balises courtes d\'ouverture)';
+$lang['CSK_SETTINGS_FILE_UPLOADS']       = 'File Uploads (transfert HTTP de fichiers)';
+$lang['CSK_SETTINGS_MAGIC_QUOTES_GPC']   = 'Magic quotes (ajout antislash aux guillemets)';
+$lang['CSK_SETTINGS_REGISTER_GLOBALS']   = 'Register Globals (EGPCS variables globales)';
+$lang['CSK_SETTINGS_OUTPUT_BUFFERING']   = 'Output Buffering (limitation du buffer de sortie)';
+$lang['CSK_SETTINGS_OPEN_BASEDIR']       = 'Open basedir (dossier limite d\'arborescence)';
+$lang['CSK_SETTINGS_SESSION.SAVE_PATH']  = 'Session Save Path (répertoire de sessions)';
+$lang['CSK_SETTINGS_SESSION.AUTO_START'] = 'Session auto start (démarrer à chaque script)';
+$lang['CSK_SETTINGS_DISABLE_FUNCTIONS']  = 'Disabled Functions (fonctions désactivées)';
+$lang['CSK_SETTINGS_XML']                = 'XML activé (lire et écrire les fichiers XML)';
+$lang['CSK_SETTINGS_ZLIB']               = 'Zlib activé (lire et écrire les fichiers gzip)';
+$lang['CSK_SETTINGS_ZIP']                = 'Zip natif activé';
+$lang['CSK_SETTINGS_MBSTRING']           = 'Mbstring actif (interprétation des chaînes)';
+$lang['CSK_SETTINGS_ICONV']              = 'Iconv activé (conversion des chaînes)	';
+$lang['CSK_SETTINGS_MAX_INPUT_VARS']     = 'Nombre maximum de champs de saisie (Maximum Input Variables)';
