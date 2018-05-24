4acf0a70ffd579fb79ae56af988a58bd8b19afbc
Kader Bouyakoub <bkader@mail.com>
2018-05-24 15:07:24 +0100

    Added query string to current URL and a new "previous_url" function.

--- skeleton/helpers/KB_url_helper.php
+++ skeleton/helpers/KB_url_helper.php
@@ -201,14 +201,54 @@ if ( ! function_exists('current_url'))
 	 * Returns the full URL (including segments) of the page where this
 	 * function is placed
 	 *
+	 * @param 	bool 	$query_string 	Whether to add QUERY STRING.
 	 * @return	string
 	 */
-	function current_url()
+	function current_url($query_string = true)
 	{
 		$CI =& get_instance();
-		$current_url = site_url($CI->uri->uri_string());
-		$params = $_SERVER['QUERY_STRING'];
-		return (empty($params)) ? $current_url : $current_url.'?'.$params;
+		$url = $CI->config->site_url($CI->uri->uri_string());
+		
+		if ($query_string && ! empty($_SERVER['QUERY_STRING']))
+		{
+			$url .= '?'.$_SERVER['QUERY_STRING'];
+		}
+
+		return $url;
+	}
+}
+
+// ------------------------------------------------------------------------
+
+if ( ! function_exists('previous_url'))
+{
+	/**
+	 * Returns the last page the user visited.
+	 *
+	 * @param 	string 	$default 	Default value if no lat page exists.
+	 * @param 	bool 	$uri_only 	Whether to return only the URI.
+	 * @return 	bool
+	 */
+	function previous_url($default = null, $uri_only = false)
+	{
+		$prev_url = $default;
+
+		if (isset($_SERVER['HTTP_REFERER']) 
+			&& $_SERVER['HTTP_REFERER'] != current_url() 
+			// We make sure the previous URL from the same site.
+			&& false !== preg_match('#^'.site_url().'#', $prev_url))
+		{
+			$prev_url = $_SERVER['HTTP_REFERER'];
+		}
+
+		if ($prev_url)
+		{
+			$prev_url = (true === $uri_only)
+				? str_replace(site_url(), '', $prev_url)
+				: site_url($prev_url);
+		}
+
+		return $prev_url;
 	}
 }
 
